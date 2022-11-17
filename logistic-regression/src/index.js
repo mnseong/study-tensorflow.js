c4cdf900e7888afb38d15522d13a819a528d909f
mnseong <mskweon@icloud.com>
2022-11-17 17:10:13 +0900

    parse diabetes.csv data

--- logistic-regression/src/index.js
+++ logistic-regression/src/index.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+import * as Papa from "papaparse";
+
+Papa.parsePromise = (file) => {
+  return new Promise((complete, error) => {
+    Papa.parse(file, {
+      header: true,
+      download: true,
+      dynamicTyping: true,
+      complete,
+      error,
+    });
+  });
+};
+
+const loadData = async () => {
+  const csv = await Papa.parsePromise(
+    "https://raw.githubusercontent.com/mnseong/study-tensorflow.js/main/logistic-regression/data/diabetes.csv"
+  );
+  return csv;
+};
+
+const run = async () => {
+  const data = await loadData();
+  console.log(data);
+};
+
+if (document.readyState !== "loading") {
+  run();
+} else {
+  document.addEventListener("DOMContentLoaded", run);
+}
