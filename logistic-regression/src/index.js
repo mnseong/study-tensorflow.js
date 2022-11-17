cdc8af41bbafee983808d40aa85f2a4ecb4056c9
mnseong <mskweon@icloud.com>
2022-11-17 17:37:55 +0900

    Feat Outcomes using Pie Chart

--- logistic-regression/src/index.js
+++ logistic-regression/src/index.js
@@ -1,4 +1,6 @@
 import * as Papa from "papaparse";
+import Plotly from "plotly.js-dist";
+import _ from "lodash";
 
 Papa.parsePromise = (file) => {
   return new Promise((complete, error) => {
@@ -16,12 +18,30 @@ const loadData = async () => {
   const csv = await Papa.parsePromise(
     "https://raw.githubusercontent.com/mnseong/study-tensorflow.js/main/logistic-regression/data/diabetes.csv"
   );
-  return csv;
+  return csv.data;
+};
+
+const renderPieChart = (data) => {
+  const outcomes = data.map((r) => r.Outcome);
+  const [diabetic, healthy] = _.partition(outcomes, (outcome) => outcome === 1);
+  const chartData = [
+    {
+      labels: ["Diabetic", "Healthy"],
+      values: [diabetic.length, healthy.length],
+      type: "pie",
+      opacity: 0.6,
+      marker: {
+        colors: ["gold", "forestgreen"],
+      },
+    },
+  ];
+  Plotly.newPlot("piechart-cont", chartData);
 };
 
 const run = async () => {
   const data = await loadData();
   console.log(data);
+  renderPieChart(data);
 };
 
 if (document.readyState !== "loading") {
